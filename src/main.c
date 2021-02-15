75c65da6db5fc6e28ed7f61090ec9bab65351a92
Kyle Smolko <ksmolko2@gmail.com>
2021-02-15 09:25:02 -0800

    Set up ethernet connection

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 #include "xil_cache_l.h"
 #include "xscugic.h"
 
+#include "netif/xadapter.h"
+#include "lwip/tcp.h"
+
 #define IMG_BUF_PTR 0x00900000
 #define NUM_BYTES_BUFFER 5242880
 
@@ -164,6 +167,29 @@ void setup()
 	memset(image_pointer[4], 0, NUM_BYTES_BUFFER);
 
 	setColours(0, image_pointer);
+
+	/************* Ethernet Setup *************/
+	ip_addr_t *ipaddr, *netmask, *gw;
+	struct netif *netif;
+	unsigned char mac_address[] = { 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
+
+	IP4_ADDR(ipaddr, 192, 168, 0, 20);
+	IP4_ADDR(netmask, 255, 255, 255, 0);
+	IP4_ADDR(gw, 192, 168, 0, 1);
+
+	lwip_init();
+
+	if (!xemac_add(netif, ipaddr, netmask, gw, mac_address, XPAR_XEMACPS_0_BASEADDR)) {
+		xil_printf("Error adding N/W interface\n\r");
+		exit(0);
+	}
+
+	netif_set_default(&netif);
+	netif_set_up(&netif);
+
+	xil_printf("IP Address: %d.%d.%d.%d\n\r", ip4_addr1(ipaddr), ip4_addr2(ipaddr), ip4_addr3(ipaddr), ip4_addr4(ipaddr));
+	xil_printf("Netmask: %d.%d.%d.%d\n\r", ip4_addr1(netmask), ip4_addr2(netmask), ip4_addr3(netmask), ip4_addr4(netmask));
+	xil_printf("Gateway: %d.%d.%d.%d\n\r", ip4_addr1(gw), ip4_addr2(gw), ip4_addr3(gw), ip4_addr4(gw));
 }
 
 void tick()
